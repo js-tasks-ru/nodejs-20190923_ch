454a1cde889a2968b0412005fba7c3af65332882
chsnt <43132492+chsnt@users.noreply.github.com>
2019-10-27 23:47:23 +0300

    commit

--- 2-module/1-task/LimitSizeStream.js
+++ 2-module/1-task/LimitSizeStream.js
@@ -4,9 +4,20 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this._limit = options.limit
+    this._size = 0
+
   }
 
   _transform(chunk, encoding, callback) {
+
+    if ( this._size + chunk.length >= this._limit ) {
+      callback(new LimitExceededError, chunk);
+    } else {
+      callback(null, chunk);
+    }
+
+    this._size += chunk.length;
 
   }
 }
